0fbaff4786e0fd8456e516de42b5df89efd8a63b
Giuseppe Angri <giuseppeangri@icloud.com>
2016-03-29 19:03:36 +0200

    Add NodeJS Controller for Metric

---
+++ controllers/metric.js
@@ -0,0 +1,36 @@
+var Metric = require("../models/metric");
+var User = require('../models/user');
+var Error = require('./error');
+
+exports.preCreate = function (req, res, next) {
+  User.findOne({token: req.headers.token}, function (err, user) {
+                  if (err || !user) { return Error.unauthorized(res); }
+                  else {
+                    if (user.admin || user.moderator) {
+                      next();
+                    }
+                  }
+                });
+};
+
+exports.preUpdate = function (req, res, next) {
+  User.findOne({token: req.headers.token}, function (err, user) {
+                  if (err || !user) { return Error.unauthorized(res); }
+                  else {
+                    if (user.admin || user.moderator) {
+                      next();
+                    }
+                  }
+                });
+};
+
+exports.preDelete = function (req, res, next) {
+  User.findOne({token: req.headers.token}, function (err, user) {
+                  if (err || !user) { return Error.unauthorized(res); }
+                  else {
+                    if (user.admin || user.moderator) {
+                      next();
+                    }
+                  }
+                });
+};
