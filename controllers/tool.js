0bae459d5ec90b6bdf8e2c0c7600208ae09f9b99
Giuseppe Angri <giuseppeangri@icloud.com>
2016-03-29 19:03:01 +0200

    Add NodeJS Controller for Tool

---
+++ controllers/tool.js
@@ -0,0 +1,17 @@
+var Tool = require('../models/tool');
+var User = require('../models/user');
+var Error = require('./error');
+
+exports.preDelete = function (req, res, next) {
+  User.findOne({token: req.headers.token}, function (err, user) {
+                  if (err || !user) { return Error.unauthorized(res); }
+                  else {
+                    if (user.admin || user.moderator) {
+                      next();
+                    }
+                    else {
+                      return Error.unauthorized(res);
+                    }
+                  }
+                });
+};
